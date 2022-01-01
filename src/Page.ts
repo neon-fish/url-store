a93832d7469a0e42fd7f4b15d9e77478851325b5
Ed <ed@neon.fish>
2022-01-01 14:06:19 +0000

    Add links between readme and live demo

--- src/Page.ts
+++ src/Page.ts
@@ -69,6 +69,9 @@ export const Page: m.Component<{}> = {
         m("h1.my-4", "URL Store"),
         m("p", `This is a test of serialising JSON into query parameters while "crushing" the JSON object to save space. The paractical limit for the length of URLs is ~2000 characters (https://stackoverflow.com/a/417184).`),
         m("p", "This may be useful for small web games/toys/apps/etc. where the application state may be saved using browser bookmarks, or shared simply as a link."),
+        m("p", [
+          m("a", { href: "https://github.com/neon-fish/url-store" }, "Github repo"),
+        ]),
         m("p", "In this test, the entered text must be valid JSON."),
       ]),
 
